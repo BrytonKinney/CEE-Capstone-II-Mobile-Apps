19866eeab707d850f1aac58c912d377abbd6c785
Bryton Kinney <brytonkinney@gmail.com>
2018-10-07 16:59:26 -0500

    Renamed project to proper name

---
+++ CapstoneApp/CapstoneApp/Models/HomeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneApp.Models
+{
+    public enum MenuItemType
+    {
+        RssFeeds,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ CapstoneApp/CapstoneApp/Views/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using CapstoneApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CapstoneApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MasterBehavior = MasterBehavior.Popover;
+
+            MenuPages.Add((int)MenuItemType.RssFeeds, (NavigationPage)Detail);
+        }
+
+        public async Task NavigateFromMenu(int id)
+        {
+            if (!MenuPages.ContainsKey(id))
+            {
+                switch (id)
+                {
+                    case (int)MenuItemType.RssFeeds:
+                        MenuPages.Add(id, new NavigationPage(new RssFeedsPage()));
+                        break;
+                    case (int)MenuItemType.About:
+                        MenuPages.Add(id, new NavigationPage(new AboutPage()));
+                        break;
+                }
+            }
+
+            var newPage = MenuPages[id];
+
+            if (newPage != null && Detail != newPage)
+            {
+                Detail = newPage;
+
+                if (Device.RuntimePlatform == Device.Android)
+                    await Task.Delay(100);
+
+                IsPresented = false;
+            }
+        }
+    }
+}
